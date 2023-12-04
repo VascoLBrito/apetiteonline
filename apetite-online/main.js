01562faafbac60ff26208d0b2b035e6959327e21
Vasco Brito <128089062+VascoLBrito@users.noreply.github.com>
2023-12-04 15:08:07 +0000

    Add files via upload

---
+++ apetite-online/main.js
@@ -0,0 +1,39 @@
+// "strict";
+
+// const parallaxElement = document.querySelector(".parallax");
+
+// window.addEventListener("scroll", function () {
+//   let offset = window.pageYOffset;
+
+//   parallaxElement.style.backgroundPositionY = offset * -1 + "px";
+// });
+
+// const icon = document.querySelector(".header-icon");
+// const icon2 = document.querySelector(".header-icon2");
+
+// const header = document.querySelector(".section-header");
+
+// header.addEventListener("mouseover", function () {
+//   icon.classList.add("display");
+//   icon2.classList.remove("display");
+
+//   header.classList.add("translate");
+// });
+// header.addEventListener("mouseout", function () {
+//   icon2.classList.add("display");
+//   icon.classList.remove("display");
+
+//   header.classList.remove("translate");
+// });
+
+// hero bg switch
+
+// const bg1 = document.querySelector(".bg-hero1");
+// const bg2 = document.querySelector(".bg-hero2");
+
+// function backgroungSwitch() {
+//   bg1.style.translate = "-50%";
+//   bg2.style.translate = "-50%";
+// }
+
+// setInterval(backgroungSwitch, 2000);
